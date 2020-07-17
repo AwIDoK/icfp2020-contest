4ba525b67150628ac31d76adf82c23da3499a4da
Sergei Kozelko <kozelkos@yandex.ru>
2020-07-17 21:48:28 +0300

    Add s, c, b, t, f, i, car, cdr, nil

--- python/interpreter.py
+++ python/interpreter.py
@@ -1,7 +1,71 @@
+# 18
+def s(x):
+    def s1(y):
+        def s2(z):
+            return x(z)(y(z))
+        return s2
+    return s1
+
+
+# 19
+def c(x):
+    def c1(y):
+        def c2(z):
+            return x(z)(y)
+        return c2
+
+    return c1
+
+
+# 20
+def b(x):
+    def b1(y):
+        def b2(z):
+            return x(y(z))
+        return b2
+
+    return b1
+
+
+#21
+def t(x):
+    def t1(y):
+        return x
+
+    return t1
+
+
+#22
+def f(x):
+    def f1(y):
+        return y
+    return f1
+
+
+#24
+def i(x):
+    return x
+
+
+#25
 def cons(x):
-    def f(y):
+    def cons1(y):
         return (x, y)
-    return f
+    return cons1
+
+
+#26
+def car(x):
+    return x[0]
+
+
+#27
+def cdr(x):
+    return x[1]
+
+
+#28
+nil = ()
 
 
 def parse(tokens):
