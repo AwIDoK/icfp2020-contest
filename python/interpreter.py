9b16d26e12c38540cf8df82b966e04efc41a24da
Andrei Filipchyk <awidokmail@gmail.com>
2020-07-18 14:50:18 +0300

    kek

--- python/interpreter.py
+++ python/interpreter.py
@@ -14,18 +14,19 @@ SEND_URL = "https://icfpc2020-api.testkontur.ru/aliens/send"
 
 
 def send(data):
+    print("SENDING")
     encoded_data = encode_alien(data)
-    print("encoded", encoded_data)
-
+    #print("encoded", encoded_data)
     params = {
         "apiKey": API_KEY
     }
     response = requests.post(SEND_URL, params=params, data=encoded_data)
+    print(response.headers)
     assert(response.status_code == 200)
 
     encoded_response = response.content.decode("utf-8")
-    print('received', encoded_response)
-    print('decoded to', decode(encoded_response)[0])
+    #print('received', encoded_response)
+    #print('decoded to', decode(encoded_response)[0])
     alien = decode_alien(encoded_response)
     return alien[0]
 
@@ -55,27 +56,31 @@ def interact(protocol_thunk):
 
 img_id = 0
 
+
 def draw(points):
-    global img_id
+    global img_id, X, Y
     img_id += 1
-    max_cell = 60
-    cell_size = 10
+    if img_id == 6:
+        raise Exception()
+    max_cell = 500
+    cell_size = 1
     side = max_cell * cell_size
     img = Image.new('RGB', (side, side))
     pixels = img.load()
+
     while isnil(points)(False)(True):
         cur_point = extract(car(points))
         points = extract(cdr(points))
         x, y = extract(car(cur_point)), extract(cdr(cur_point))
 
-        x += 30
-        y += 30
+        x += 250
+        y += 250
 
         assert (x >= 0 and y >= 0 and x < max_cell and y < max_cell)
         for xi in range(x * cell_size, x * cell_size + cell_size):
             for yi in range(y * cell_size, y * cell_size + cell_size):
                 pixels[xi, yi] = (255, 255, 255)
-    img.save("{}.png".format(img_id), "PNG")
+    img.save("{}_{}_{}.png".format(X, Y, img_id), "PNG")
 
 
 def multipledraw(images):
@@ -135,7 +140,7 @@ def run_interact(coord1, coord2, state, function_thunk_dict):
     prev_state = ''
     new_state = encode_alien(extract(state))
     cur_state = state
-
+    print(decode(new_state))
     while prev_state != new_state:
         def run(x):
             return interact(evaluate(function_thunk_dict['galaxy'], function_thunk_dict))(cur_state)(cons(coord1)(coord2))
@@ -143,10 +148,14 @@ def run_interact(coord1, coord2, state, function_thunk_dict):
         prev_state = new_state
         cur_state = evaluate((run,), function_thunk_dict)
         new_state = encode_alien(cur_state)
+        print(decode(new_state))
 
+X = 0
+Y = 0
 
 def main():
-    file = open('galaxy.txt', 'r')
+    global X, Y
+    file = open('..\galaxy.txt', 'r')
     function_thunk_dict = {}
     for line in file:
         if line[-1] == "\n":
@@ -156,9 +165,20 @@ def main():
         parsed = parse(split[1].strip().split(" "))
         assert(parsed[1] == [])
         function_thunk_dict[name] = parsed[0]
-
-    run_interact(0, 0, nil, function_thunk_dict)
-
+    for i in range(-100, 101, 5):
+        for j in range(-100, 101, 5):
+            global img_id
+            img_id = 0
+            print("processing", X, Y)
+            X = i
+            Y = j
+            try:
+                run_interact(X, Y, decode_alien('110110001011110110000111101000010011010110000')[0], function_thunk_dict)
+            except AssertionError as e:
+                print("ERROR", X, Y, e)
+            except Exception as e:
+                pass
+            memorized = {}
 
 if __name__ == "__main__":
     main()
